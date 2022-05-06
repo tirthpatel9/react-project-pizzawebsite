20763ad61de34e906e379ca67604bce3e54c9ca2
tirthpatel9 <60367431+tirthpatel9@users.noreply.github.com>
2022-05-06 15:21:28 -0600

    starting state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Basket from "./components/Basket";
 function App() {
   return (
     <div className="App">
@@ -12,3 +11,23 @@ function App() {
 }
 
 export default App;
+
+{/*import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       basket:[]
+    }
+  }
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App*/}
+
